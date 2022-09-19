ac8ad777129c080ed2f6acef882381b5adcc4f13
regular1020 <pjg1020@kookmin.ac.kr>
2022-09-19 20:02:39 +0900

    feat: Add signup page
    Add signup page
    Add signup activity

--- app/src/main/java/com/harmony/harmonyAndroid/MainActivity.kt
+++ app/src/main/java/com/harmony/harmonyAndroid/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.harmony.harmonyAndroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.harmony.R
+import com.example.harmony.databinding.ActivityMainBinding
+import com.harmony.harmonyAndroid.sign_up.SignUpActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBinding: ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnNavigateSignup.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        // onDestroy 에서 binding class 인스턴스 참조를 정리해주어야 한다.
+        mBinding = null
+        super.onDestroy()
     }
 }
