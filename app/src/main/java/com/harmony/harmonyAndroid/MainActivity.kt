0e0abbe83bc42b8a13b57b48411575959fa58971
regular1020 <pjg1020@kookmin.ac.kr>
2022-10-31 22:00:32 +0900

    feat: Add FCM reciver
    Add message recive method
    Add notification method

--- app/src/main/java/com/harmony/harmonyAndroid/MainActivity.kt
+++ app/src/main/java/com/harmony/harmonyAndroid/MainActivity.kt
@@ -3,6 +3,8 @@ package com.harmony.harmonyAndroid
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessaging
 import com.harmony.harmonyAndroid.activity.MessageActivity
 import com.harmony.harmonyAndroid.databinding.ActivityMainBinding
 
@@ -12,6 +14,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Log.d("token", task.result)
+            }
+        }
 
         binding.btnMessage.setOnClickListener {
             val intent = Intent(this, MessageActivity::class.java)
